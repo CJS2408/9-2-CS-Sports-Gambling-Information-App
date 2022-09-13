6cab986a2358db6b676f12b00c5f067e03691eda
cjs2408 <carlossuero@pursuit.org>
2022-09-12 20:31:59 -0500

    add-ons

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'e582372e1amsh61b00d4f0c7496fp13ecf4jsn15d9fbef3010',
-		'X-RapidAPI-Host': 'mlb-data.p.rapidapi.com'
-	}
-};
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'e582372e1amsh61b00d4f0c7496fp13ecf4jsn15d9fbef3010',
+// 		'X-RapidAPI-Host': 'mlb-data.p.rapidapi.com'
+// 	}
+// };
 
-fetch('https://mlb-data.p.rapidapi.com/json/named.player_info.bam?sport_code='mlb'&player_id='493316'', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+// fetch("https://mlb-data.p.rapidapi.com/json/named.player_info.bam?sport_code='mlb'&player_id='493316'", options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
 
-  function displayPlayerData(data) {
-    const player = data.drinks[0];
-    const playerDiv = document.getElementById("Player"); 
-  }   
+//   function displayPlayerData(data) {
+//     const player = data.drinks[0];
+//     const playerDiv = document.getElementById("Player"); 
+//   }   
 
-  const playerName = player.id;
-  const heading = document.createElement("h1");
-  heading.innerHTML = playerName;
+//   const playerName = player.id;
+//   const heading = document.createElement("h1");
+//   heading.innerHTML = playerName;
 
   
+
+  fetch("https://mlb-data.p.rapidapi.com/json/named.player_info.bam?sport_code='mlb'&player_id='493316'")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data);
+    displayPlayerData(data)
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
