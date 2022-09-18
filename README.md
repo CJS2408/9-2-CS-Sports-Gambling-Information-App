ceeafa0c305d5aafa167801bc39d86028e3f9f3c
cjs2408 <carlossuero@pursuit.org>
2022-09-18 16:00:30 -0500

    Upload Part 2. Fixed index.html login page

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ An app for sports statistics used in the most common gambling scenarios.
 2. Open the directory through your terminal and install all packages associated.
 3. Use the command "Code ." to open up the project on VS Code.
 4. Once the project is open you are able to open up all associated files.
-5. Modify and Enjoy!
+5. When the project is downloaded, you can deploy to the web. 
+6. You will land on the login page (Username & Password aren't working yet, so please just press the "Login" button)
+7. You will now be transferred to the main page. 
+8. Enjoy the gambling stats!
--- main.js
+++ main.js
@@ -1,33 +1,26 @@
 
 
-function myButton(){
 
-const options = {
+	const options = {
 		method: 'GET',
 		headers: {
-			'X-RapidAPI-Key': '732a40fa19msh7836d44be03a72fp1458f8jsnb4cc84dfe36b',
+			'X-RapidAPI-Key': 'e582372e1amsh61b00d4f0c7496fp13ecf4jsn15d9fbef3010',
 			'X-RapidAPI-Host': 'baseballapi.p.rapidapi.com'
-		}
-	};
-	
-	fetch('https://baseballapi.p.rapidapi.com/api/baseball/match/9864379', options)
-		.then(response => response.json())
-		.then(response => console.log(response))
-		.catch(err => console.error(err));
-
-
-		const options1 = {
-			method: 'GET',
-			headers: {
-				'X-RapidAPI-Key': 'e582372e1amsh61b00d4f0c7496fp13ecf4jsn15d9fbef3010',
-				'X-RapidAPI-Host': 'baseballapi.p.rapidapi.com'
 			}
 		};
-		
-		fetch('https://baseballapi.p.rapidapi.com/api/baseball/player/977489/statistics/seasons', options)
-			.then(response => response.json())
-			.then(response => console.log(response))
-			.catch(err => console.error(err));
-
-				
-			}
+	const fetchDataBtn = document.querySelector('#fetchdata')
+    const result = document.querySelector('#result')
+    
+    
+    const getData = function() {
+      result.innerText = 'Loading....'
+      fetch('https://baseballapi.p.rapidapi.com/api/baseball/match/9864379', options)
+        .then(response => response.json())
+        .then(data => {
+          result.innerText = JSON.stringify(data, null, 2)
+        })
+        .catch(error => console.log(error))
+    }
+    
+  
+    // fetchDataBtn.addEventListener('click', getData)
